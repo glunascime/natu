dfb4f5a6e7d686ff56110c61296fb1e792de7eed
GLN <g.luana@hotmail.com>
2021-05-04 18:48:01 -0300

    Create addToCart function + add shopping cart counter (user area)

---
+++ public/scripts/cart.js
@@ -0,0 +1,28 @@
+let cartItems = [];
+
+function addToCart(shopButton) {
+
+    let productId = shopButton.id;
+
+    let product = {
+        "name": document.getElementById(productId+"Name").innerText,
+        "price": parseInt(document.getElementById(productId+"Price").innerText.replace("R$", ""))
+    }
+
+    cartItems.push(product);
+    document.getElementById("itemCounter").innerHTML = cartItems.length;
+
+    let emptyBag = document.getElementById("emptyBag");
+    let fullBag = document.getElementById("fullBag");
+
+    if (cartItems.length > 0) {
+        emptyBag.classList.add("hide");
+        fullBag.classList.remove("hide");
+    } else { 
+        emptyBag.classList.remove("hide");
+        fullBag.classList.add("hide");
+    }
+
+    console.log(cartItems);
+}
+
